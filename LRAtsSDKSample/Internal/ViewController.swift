29d365541909d11efcef7b17158d1d3945bc81c8
Najdan Tomic <90030627+najdan-liveramp@users.noreply.github.com>
2024-02-09 15:59:50 +0100

    Initial setup
    * Add LICENSE and README
    
    * Add LRAtsSDKSample project

---
+++ LRAtsSDKSample/Internal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LRAtsSDKSample
+//
+//  Created by Najdan Tomic on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
