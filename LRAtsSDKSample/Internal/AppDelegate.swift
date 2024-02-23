4f2f451d7c3952ee72da14cc23fd867dd47e2fe7
meris-lr <117715267+meris-lr@users.noreply.github.com>
2024-02-23 12:10:18 +0100

    PMSD-2479 - Add envelope flow screen (#2)
    Add envelope flow screen

--- LRAtsSDKSample/Internal/AppDelegate.swift
+++ LRAtsSDKSample/Internal/AppDelegate.swift
@@ -4,27 +4,40 @@
 //
 //  Created by Najdan Tomic on 2/9/24.
 //
+//  Copyright © 2024 Liveramp. All rights reserved.
+//
 
 import UIKit
+import AppTrackingTransparency
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window:UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Make sure you have ATT consent and user consent to initialize and get envelope successfully
+        ConsentManager.simulateUserConsent()
+        ATTrackingManager.requestTrackingAuthorization { status in
+            switch status {
+            case .authorized:
+                // Tracking authorization dialog was shown and we are authorized
+                print("Authorized")
+            case .denied:
+                // Tracking authorization dialog was shown and permission is denied
+                print("Denied")
+            case .notDetermined:
+                // Tracking authorization dialog has not been shown
+                print("Not Determined")
+            default:
+                print("Unknown")
+            }
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
-
 }
-
